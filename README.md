7dc2a8d4ed36a8be0e9eddf854534c2f93690ec4
William Buckingham <william.buckingham.contractor@macmillan.com>
2016-11-03 15:51:21 -0400

    Updated Readme with references. return routes on main api endpoint, added support for insert and update delete.

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # node-rest-mysql
 
-This is a boilerplate for using Node.js with Express to host a RESTful API for a MySQL/MariaDB Database.
+This is a boilerplate with proper for using Node.js with Express to host a RESTful API for a MySQL/MariaDB Database.
+
+## Resources
+
+Developed using a couple resources:
+
+1. Express (http://expressjs.com/en/4x/api.html)
+2. MySQL for Node (https://github.com/mysqljs/mysql)
+3. RESTAPI Tutorial (http://www.restapitutorial.com/lessons/httpmethods.html)
+4. Build a RESTful API Using Node and Express 4 (https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4)
+5. Node 404 (http://stackoverflow.com/a/33310600)
+6. RESTful Best Practices (http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#advanced-queries)
+7. Test a Node RESTful API with Mocha and Chai (https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai)
 
 ## Installation
 
@@ -17,6 +29,6 @@ This is a boilerplate for using Node.js with Express to host a RESTful API for a
    }
    ```
 
-3. asdf
+3. OR pass in MYSQL_HOST, etc as evnironment variables (eg. from Heroku's Config Vars)
 
-4. ​
+4. Use Postman or your browser to make requests to the api, at `/api/…`
--- app.js
+++ app.js
@@ -51,13 +51,8 @@ router.use(function(req, res, next) {
     next();
 });
 
-// api routes
+// API Routes
 router.get('/', function(req, res) {
-    res.jsonp({
-        name: 'Panorama API', 
-        version: '1.0'
-    });
-
     //Generate a List of Routes on the APP
     var route, routes = [];
     app._router.stack.forEach(function(middleware){
@@ -71,8 +66,16 @@ router.get('/', function(req, res) {
         }
     });
     console.log(routes)
+
+    res.jsonp({
+        name: 'Panorama API', 
+        version: '1.0',
+        routes: routes
+    });
+
 });
 
+// Simple MySQL Test
 router.get('/test', function(req, res) {
     var test;
     
@@ -91,15 +94,27 @@ router.get('/test', function(req, res) {
 // http://www.restapitutorial.com/lessons/httpmethods.html
 // POST - Create
 // GET - Read
-// PUT - Update/Replace
-// PATCH - Update/Modify
+// PUT - Update/Replace - AKA you pass all the data to the update
+// PATCH - Update/Modify - AKA you just pass the changes to the update
 // DELETE - Delete
 
 // COLLECTION ROUTES
 router.route('/panoramas')
     //we can use .route to then hook on multiple verbs
     .post(function(req, res) {
-        //todo
+        var data = {
+            //data to post, assemble from params???
+        }
+        var query = connection.query('INSERT INTO panos SET ?', [data], function(err, result){
+            if(err){
+                console.error(err);
+                res.sendStatus(404);
+            }else{
+                res.status(201);
+                res.location('/api/panoramas/' + result.insertedId)
+            }
+        });
+        console.log(query.sql);
     })
 
     .get(function(req, res) {
@@ -119,14 +134,15 @@ router.route('/panoramas')
         res.sendStatus(404);
     })
     .delete(function(req, res) {
+        // LET's TRUNCATE TABLE..... NOT!!!!!
         res.sendStatus(404);
     });
 //end route
 
-// INDIVIDUAL ROUTES
+// SPECIFIC ITEM ROUTES
 router.route('/panoramas/:id')
     .post(function(req, res){
-        //
+        //specific item should not be posted to (either 404 not found or 409 conflict?)
         res.sendStatus(404);
     })
 
@@ -136,16 +152,47 @@ router.route('/panoramas/:id')
                 //INVALID
                 console.error(err);
                 res.sendStatus(404);
-            }
-            if(rows.length){
-                res.jsonp(rows);
             }else{
-                //ID NOT FOUND
-                res.sendStatus(404);
+                if(rows.length){
+                    res.jsonp(rows);
+                }else{
+                    //ID NOT FOUND
+                    res.sendStatus(404);
+                }
             }
         });
         console.log(query.sql);
+    })
+
+    .put(function(req, res){
+        var data = {
+            //data to put, assemble from params???
+        };
+        var query = connection.query('UPDATE panos SET ? WHERE id=? LIMIT 1', [data, req.params.id], function(err, results){
+            if(err){
+                console.log(err);
+                res.sendStatus(404);
+            }else{
+                res.status(200).jsonp({changedRows:result.changedRows, affectedRows:result.affectedRows}).end();
+            }
+        })
+    })
+
+    .patch(function(req, res){
+        // Need to decide how much this should differ from .put
+    })
+
+    .delete(function(req, res){
+        var query = connection.query('DELETE FROM panos WHERE id=? LIMIT 1', [req.params.id], function(err, results){
+            if(err){
+                console.log(err);
+                res.sendStatus(404);
+            }else{
+                res.status(200).jsonp({affectedRows:result.affectedRows}).end();
+            }
+        });
     });
+//end route
 
 // Apply Routes to App
 // All of these routes will be prefixed with /api
