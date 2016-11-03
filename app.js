3612292597d3d18b48c066b57e42fc1ff380d080
Will <will.buckingham@gmail.com>
2016-11-02 23:51:16 -0400

    improved route middleware etc

--- app.js
+++ app.js
@@ -21,22 +21,45 @@ app.use(express.static('public'));
 
 connection.connect();
 
-//router middleware
-//we can use this later to validate some stuff
+// Router Middleware
 router.use(function(req, res, next) {
     // log each request to the console
     console.log("You have hit the /api", req.method, req.url);
+
+    // Remove powered by header
+    // res.set('X-Powered-By', ''); // OLD WAY
+    // res.removeHeader("X-Powered-By"); // OLD WAY 2
+    // See bottom of script for better way
+
+    // CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // we can use this later to validate some stuff
 
     // continue doing what we were doing and go to the route
     next();
 });
 
-//api routes
+// api routes
 router.get('/', function(req, res) {
     res.json({
         name: 'Panorama API', 
         version: '1.0'
     });
+
+    var route, routes = [];
+    app._router.stack.forEach(function(middleware){
+        if(middleware.route){ // routes registered directly on the app
+            routes.push(middleware.route);
+        } else if(middleware.name === 'router'){ // router middleware 
+            middleware.handle.stack.forEach(function(handler){
+                route = handler.route;
+                route && routes.push(route);
+            });
+        }
+    });
+    console.log(routes)
 });
 
 router.get('/test', function(req, res) {
@@ -54,6 +77,12 @@ router.get('/test', function(req, res) {
     }); 
 });
 
+//POST - Create
+//GET - Read
+//PUT - Update/Replace
+//PATCH - Update/Modify
+//DELETE - Delete
+
 //we can use .route to then hook on multiple verbs
 router.route('/panoramas')
     .get(function(req, res) {
@@ -65,35 +94,48 @@ router.route('/panoramas')
     })
 
     .post(function(req, res) {
+        //todo
+    })
 
+    //We do NOT do these to the collection
+    .put(function(req, res) {
+        //res.status(404).send("Not Found").end();
+        res.sendStatus(404);
+    })
+    .patch(function(req, res) {
+        res.sendStatus(404);
+    })
+    .delete(function(req, res) {
+        res.sendStatus(404);
     });
+
 //end route
 
 router.route('/panoramas/:id')
     .get(function(req, res) {
-        console.log(req.id);
-        var query = connection.query('SELECT * FROM panos WHERE id=?', [req.id], function(err, rows, fields) {
+        var query = connection.query('SELECT * FROM panos WHERE id=?', [req.params.id], function(err, rows, fields) {
             if (err) console.error(err);
-            console.log(rows);
-            res.json(rows);
+            if(rows.length){
+                res.json(rows);
+            }else{
+                res.sendStatus(404);
+            }
         });
-        console.log(query);
+        console.log(query.sql);
     });
 
-//apply routes to app
-//all of our routes will be prefixed with /api
+// Apply Routes to App
+// All of these routes will be prefixed with /api
 app.use('/api', router);
 
-//non api route for our views
+// non api route for our views
 app.get('/', function(req, res) {
     res.render('index');
 });
 
 
-
-
-
-
+// Better way to disable x-powered-by
+app.disable('x-powered-by');
 
 
 
